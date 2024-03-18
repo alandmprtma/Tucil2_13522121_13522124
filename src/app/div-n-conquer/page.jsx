0b7e2ebfc112d67d79a2b42cf1e14efd720ee98b
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2024-03-18 19:05:42 +0700

    structure: structuring program

--- src/app/div-n-conquer/page.jsx
+++ src/app/div-n-conquer/page.jsx
@@ -133,7 +133,8 @@ function PointsInput({mode, numPoints, setInputPoints, setIteration, numIteratio
     {/* Input untuk jumlah iterasi */}
     <div className='ml-4 space-x-4 space-y-2'>
       <label>Number of Iterations:</label>
-      <input type="number" className="bg-white text-black p-1 rounded border border-gray-300 w-[75px]" value={numIterations} onChange={(e) => setIteration(Number(e.target.value))}/>
+      <input type="number" className="bg-white text-black p-1 rounded border border-gray-300 w-[75px]" 
+      value={numIterations} onChange={(e) => setIteration(Number(e.target.value))}/>
     </div>
     {/*  Input jumlah poin yang diinginkan (hanya untuk mode 'N-Points')  */}
     </div>
@@ -166,10 +167,13 @@ export default function DivideConquer() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b 
+        from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit 
+        lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <code className="font-mono font-bold">Divide and Conquer Algorithms</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white 
+        via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
             <Image
               src="/curving.png"
@@ -182,12 +186,21 @@ export default function DivideConquer() {
           </a>
         </div>
       </div>
-      <div className="relative flex lg:flex-row md:flex-col sm:flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[1000px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[800px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[1]">
-        <div className="m-[25px] h-[700px] border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-[700px]  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="relative flex lg:flex-row md:flex-col sm:flex-col place-items-center before:absolute before:h-[300px] 
+      before:w-full sm:before:w-[1000px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white 
+      before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full 
+      sm:after:w-[800px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl 
+      after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 
+      before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[1]">
+        <div className="m-[25px] h-[700px] border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl 
+        dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-[700px]  lg:rounded-xl lg:border 
+        lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <p className="mb-3 text-xl font-semibold">Bezier Curve Ilustration</p>
           {bezierPoints.length > 0 && <DivideConquerClient points={bezierPoints} inputPoints={inputPoints}/>}
         </div>
-        <div className="m-[25px]  z-50 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-[400px]  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 h-fit">
+        <div className="m-[25px]  z-50 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl 
+        dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-[400px]  lg:rounded-xl lg:border 
+        lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 h-fit">
           <div className="m-4 p-4">
             <h2 className="text-xl font-semibold">Input Points</h2>
             <form onSubmit={handleSubmit}>
@@ -213,7 +226,8 @@ export default function DivideConquer() {
         <div className="lg:w-{150} lg:h-{75} md:w-{150} md:h-{75} sm:w-{150} sm:h-{75}"></div>
         <a
           href="/brute-force"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 
+          hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Brute Force{" "}
@@ -228,7 +242,8 @@ export default function DivideConquer() {
 
         <a
           href="/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 
+          hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
           style={{ width: '275px' }}
         >
